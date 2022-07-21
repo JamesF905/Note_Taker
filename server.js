55accc58d65cbb5b2e3eac95e646997701a99588
James <jameslfidlin@gmail.com>
2022-07-20 23:44:22 -0400

    updated

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const db_data = require('./db/db.json');
 
 const path = require('path');
 
@@ -16,9 +17,7 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'db/db.json'))
-);
+app.get('/api/notes', (req, res) => res.json(db_data));
 
 app.post('/api/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'db/db.json'))
