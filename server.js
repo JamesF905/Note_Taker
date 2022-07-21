1e4ec63d6bf3aeafdbf8f4e1d4841a8547e3762f
James <jameslfidlin@gmail.com>
2022-07-20 23:37:44 -0400

    updated

--- server.js
+++ server.js
@@ -4,18 +4,28 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3002;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-app.get('/paths', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/paths.html'))
+app.get('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'db/db.json'))
+);
+
+app.post('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'db/db.json'))
+);
+
+app.delete('/api/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'db/db.json'))
 );
 
 app.listen(PORT, () =>
